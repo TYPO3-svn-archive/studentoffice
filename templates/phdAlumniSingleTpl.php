cc462dc0d7012c9e2c839fa530aed103d2ca589b
skom <skom@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-06 11:55:17 +0000

    Detail-View for alumni
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/studentoffice/trunk@35416 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ templates/phdAlumniSingleTpl.php
@@ -0,0 +1,81 @@
+<?php if (!defined ('TYPO3_MODE')) 	die ('Access denied.'); ?>
+<?php
+/*******************************************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2009 Sven Kalbhenn
+ *  Contact: sven@skom.de
+ *  All rights reserved
+ *
+ *  If you ar not familiar with php-Templates, you can find a documentation here:
+ *  http://typo3.org/documentation/document-library/extension-manuals/lib/0.0.20/view/1/5/
+ *******************************************************************************************/
+ ?>
+
+ 
+ <?php if($this->isNotEmpty()): ?>
+	<table width="100%" border="0" cellpadding="0" cellspacing="0" class="studentList">
+		<tr>
+			<td rowspan="7" class="student_links" valign="top">
+				<?php $this->printStudentImg(); ?>
+			</td>
+			<td class="student_rechts" valign="top">
+				%%%name%%%:
+			</td>
+			<td class="student_rechts" valign="top">
+				<?php //t3lib_div::debug($entry,'exampleAction'); ?>
+				<strong><?php $this->printAsText('lastname'); ?>, <?php $this->printAsText('firstname'); ?></strong>
+			</td>
+		</tr>
+		<tr>
+			<td class="student_rechts" valign="top">
+				%%%citizenship%%%:
+			</td>
+			<td class="student_rechts" valign="top">
+				<?php $this->printAsText('nationality'); ?>
+			</td>
+		</tr>
+		<tr>
+			<td class="student_rechts" valign="top">
+				%%%affiliation%%%:
+			</td>
+			<td class="student_rechts" valign="top">
+				<?php $this->printAsText('paffiliation'); ?>
+			</td>
+		</tr>
+		<tr>
+			<td class="student_rechts" valign="top">
+				%%%supervisor%%%:
+			</td>
+			<td class="student_rechts" valign="top">
+				<?php $this->printAsText('psupervisor'); ?>
+			</td>
+		</tr>
+		<tr>
+			<td class="student_rechts" valign="top">
+				%%%thesisCommittee%%%:
+			</td>
+			<td class="student_rechts" valign="top">
+				<?php $this->printAsText('pcommitee'); ?>
+			</td>
+		</tr>
+		<tr>
+			<td class="student_rechts" valign="top">
+				%%%dissertation%%%:
+			</td>
+			<td class="student_rechts" valign="top">
+				<?php $this->printAsText('pdissertation'); ?> 
+			</td>
+		</tr>
+		<tr>
+			<td class="student_rechts" valign="top">
+				%%%publications%%%:
+			</td>
+			<td class="student_rechts" valign="top">
+				<?php $this->printAsRte('ppublications'); ?>
+			</td>
+		</tr>
+	</table>
+<?php endif; ?>
+
+
